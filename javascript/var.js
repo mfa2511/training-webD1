0ddc646be402a345a3b407918a246961c635b3ad
MD FATEH ALAM <mdfatehalam25112002@gmail.com>
2025-04-14 18:27:15 +0530

    add

---
+++ javascript/var.js
@@ -0,0 +1,33 @@
+var a =5;
+var b=6;
+var c=7;
+var d="fateh"
+let r=67
+console.log(a+b*c)
+console.log(d)
+console.log(typeof a,typeof b, typeof d)
+{
+   let r=54
+    console.log(r)
+}
+
+let x="fateh"
+let y=34
+let z=3.55
+const p=true
+let q=undefined
+let s=null
+
+console.log(x,y,z,p,q,s)
+console.log(typeof x, typeof y,typeof z, typeof p,typeof q,typeof s)
+
+let o={
+    "name": "fateh",
+    "job code": 6500,
+    is_handsum: true
+}
+console.log(o)
+o.salary = "100 crores"
+console.log(o)
+o.salary="500 crores"
+console.log(o)
